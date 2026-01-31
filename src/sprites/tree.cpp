5aa1972e1ed1caac649b64933455f99601a30472
hooss-only <hoossonly@gmail.com>
2026-01-31 14:54:47 +0900

    Delete useless log

--- src/sprites/tree.cpp
+++ src/sprites/tree.cpp
@@ -28,15 +28,12 @@ void Tree::on_click(Uint8 button) {
                 float x = 0;
                 float d = (float) this->max_hp / SHEET_AMOUNT;
 
-                SDL_LogDebug(SDL_LOG_CATEGORY_TEST, "## Start sheet calculate");
                 for (int i=0; i<SHEET_AMOUNT; i++) {
                         SDL_LogDebug(SDL_LOG_CATEGORY_TEST, "%d %f", hp, d*i);
                         if ((float) this->hp > d * i) {
                                 x = (SHEET_AMOUNT - i - 1) * SPRITE_WIDTH;
                         }
                 }
-
-                SDL_LogDebug(SDL_LOG_CATEGORY_TEST, "## %f %f %d", x, d, hp);
 
                 this->texture->set_src({ x, 0, SPRITE_WIDTH, SPRITE_HEIGHT });
         }
